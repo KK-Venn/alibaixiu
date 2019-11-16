3725dbc896f4dadb32933c62106a61a6ef14bdc0
KK-Venn <3028074164@qq.com>
2019-11-16 16:01:53 +0800

    文章封面即时预览完成

--- public/js/post-add.js
+++ public/js/post-add.js
@@ -3,7 +3,26 @@ $.ajax({
     type: "get",
     url: "/categories",
     success: function (response) {
-        var html = template('categoryTpl', {data: response});
+        var html = template('categoryTpl', {
+            data: response
+        });
         $('#category').html(html);
     }
 });
+//文章封面上传预览功能
+$('#feature').on('change', function () {
+    var formData = new FormData();
+    formData.append('cover', this.files[0]);
+    $.ajax({
+        type: "POST",
+        url: "/upload",
+        contentType: false,
+        processData: false,
+        data: formData,
+        success: function (response) {
+            $('#thumbnail').val(response[0].cover);
+            $("#preview").show();
+            $("#preview").attr('src', response[0].cover);
+        }
+    });
+})
