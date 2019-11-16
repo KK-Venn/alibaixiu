1a7b5d0d9936ff024ec464ff06221feca1ad3eeb
KK-Venn <3028074164@qq.com>
2019-11-16 14:59:21 +0800

    分类数据修改完成

--- public/js/category.js
+++ public/js/category.js
@@ -3,17 +3,44 @@ $.ajax({
     type: "get",
     url: "/categories",
     success: function (response) {
-        var html = template('categoryListTpl',{data:response});
+        var html = template('categoryListTpl', {
+            data: response
+        });
         $('#categoryBox').html(html);
     },
 });
-
 //添加分类
-$('#addCategory').on('submit',function(){
+$('#addCategory').on('submit', function () {
     var data = $(this).serialize();
     $.ajax({
         type: "post",
         url: "/categories",
+        data: data,
+        success: function (response) {
+            location.reload();
+        }
+    });
+    return false;
+});
+//获取要修改的分类数据
+$('#categoryBox').on('click', '.edit', function () {
+    var id = $(this).attr('data-id');
+    $.ajax({
+        type: "get",
+        url: `/categories/${id}`,
+        success: function (response) {
+            var html = template('categoryModifyTpl', response);
+            $('#modifyBox').html(html);
+        }
+    });
+});
+//修改分类数据
+$('#modifyBox').on('submit', '#ModifyCategory', function () {
+    var data = $(this).serialize();
+    var id = $(this).attr('data-id');
+    $.ajax({
+        type: "put",
+        url: "/categories/" + id,
         data: data,
         success: function (response) {
             location.reload();
