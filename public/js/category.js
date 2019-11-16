05fde2418701f1764c6818666964293e84767e50
KK-Venn <3028074164@qq.com>
2019-11-16 14:29:52 +0800

    分类列表展示完成

--- public/js/category.js
+++ public/js/category.js
@@ -1,3 +1,14 @@
+//获取分类数据
+$.ajax({
+    type: "get",
+    url: "/categories",
+    success: function (response) {
+        var html = template('categoryListTpl',{data:response});
+        $('#categoryBox').html(html);
+    },
+});
+
+//添加分类
 $('#addCategory').on('submit',function(){
     var data = $(this).serialize();
     $.ajax({
