92cf7e464666c28ef749d1bd6c63672bd86c30c1
KK-Venn <3028074164@qq.com>
2019-11-16 10:50:46 +0800

    删除多个用户功能完成

--- public/js/user.js
+++ public/js/user.js
@@ -62,7 +62,7 @@ $('#modifyBox').on('submit', '#modifyForm', function () {
     });
     return false;
 });
-//删除用户
+//删除单个用户
 $('#userbox').on('click', '.delete', function () {
     if (confirm('您真的要删除此用户吗？')) {
         var id = $(this).attr('data-id');
@@ -74,4 +74,53 @@ $('#userbox').on('click', '.delete', function () {
             }
         });
     };
+});
+//删除多个用户
+$('#deleteMany').on('click', function () {
+    var ids = [];
+    var checkUser = $('#userbox').find('input').filter(':checked');
+    checkUser.each(function (index, element) {
+        ids.push($(element).attr('data-id'));
+    });
+    if (confirm('您确定要进行批量删除操作吗？')) {
+        $.ajax({
+            type: "delete",
+            url: "/users/" + ids.join('-'),
+            success: function (response) {
+                location.reload();
+            },
+            error:function(){
+                alert('批量删除失败');
+                location.reload();
+            }
+        });
+    } else {
+        return false;
+    }
+});
+//从全部复选框同步到以下单选框
+$('#selectAll').on('change', function () {
+    var status = $(this).prop('checked');
+    if (status) {
+        $('#deleteMany').show();
+    } else {
+        $('#deleteMany').hide();
+    }
+    //获取所有用户并同步全选框状态
+    $('#userbox').find("input").prop('checked', status);
+});
+//从用户前复选框状态同步到全选框
+$('#userbox').on('change', '.userStatus', function () {
+    //判断选中的用户数量是否与用户总数一致，一致则将全选框选中，否则将全选框放开
+    if ($('.userStatus').length == $('.userStatus:checked').length) {
+        $('#selectAll').prop('checked', true);
+    } else {
+        $('#selectAll').prop('checked', false);
+    };
+    //显示批量删除按钮
+    if ($('.userStatus:checked').length) {
+        $('#deleteMany').show();
+    } else {
+        $('#deleteMany').hide();
+    }
 });
