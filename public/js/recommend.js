e0026ec67ffcd60d934b27d8a6ea6dcdf7ab467d
KK-Venn <3028074164@qq.com>
2019-11-19 09:47:39 +0800

    热门推荐数据获取

---
+++ public/js/recommend.js
@@ -0,0 +1,18 @@
+//向服务器端发送请求所要热门推荐数据
+$.ajax({
+    type: "get",
+    url: "/posts/recommend",
+    success: function (response) {
+        var recommendTpl = `
+        {{each data}}
+        <li>
+        <a href="detail.html?id={{$value._id}}">
+          <img src="{{$value.thumbnail}}" alt="">
+          <span>{{$value.title}}</span>
+        </a>
+      </li>
+      {{/each}}`;
+      var html = template.render(recommendTpl,{data:response});
+      $('#recommendBox').html(html);
+    }
+});
