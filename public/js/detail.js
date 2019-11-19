ad2c40f36bd0d21126dfefb0f7b0dcbe98b4f35c
KK-Venn <3028074164@qq.com>
2019-11-19 15:52:14 +0800

    点赞功能完成

--- public/js/detail.js
+++ public/js/detail.js
@@ -3,7 +3,18 @@ $.ajax({
     type: "get",
     url: "/posts/" + id,
     success: function (response) {
-        var html = template('postTpl',response);
+        var html = template('postTpl', response);
         $('#postBox').html(html);
     }
+});
+//点赞功能
+$('#postBox').on('click', '#click', function () {
+    $.ajax({
+        type: "post",
+        url: "/posts/fabulous/" + id,
+        success: function () {
+            alert('点赞成功，感谢您的支持');
+            location.reload();
+        }
+    });
 });
